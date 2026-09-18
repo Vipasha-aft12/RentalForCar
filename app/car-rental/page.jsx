9d9838d0db019ede7828b57d8b905e1a85571df1
Vipasha Sharma <vipasha@affluencetravels.com>
2026-09-18 18:58:11 +0530

    Fix CarRental->carrental import case for Linux build

--- app/car-rental/page.jsx
+++ app/car-rental/page.jsx
@@ -1,7 +1,7 @@
 import { getPageMeta } from '@/lib/seo';
-import AirportPickUp from '@/components/CarRental/AirportPickUp/AirportPickUp';
-import Guides from '@/components/CarRental/Guides/Guides';
-import OneCallNoCounterSurprises from '@/components/CarRental/OneCallNoCounterSurprises/OneCallNoCounterSurprises';
+import AirportPickUp from '@/components/carrental/AirportPickUp/AirportPickUp';
+import Guides from '@/components/carrental/Guides/Guides';
+import OneCallNoCounterSurprises from '@/components/carrental/OneCallNoCounterSurprises/OneCallNoCounterSurprises';
 
 export function generateMetadata() {
   return getPageMeta('/car-rental/');
